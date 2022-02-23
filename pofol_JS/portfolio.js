ca15359139e7ffb1e382abb720fb724e88504c68
cjho0919 <85872781+cjho0919@users.noreply.github.com>
2022-02-24 00:59:14 +0900

    Update portfolio.js

--- pofol_JS/portfolio.js
+++ pofol_JS/portfolio.js
@@ -133,9 +133,9 @@ function pro(id, per){
     const $window = $(".window");
     const $window_content = $(".window-content");
     const img_list = [
-        "../portfolio/img/ERD.png",
-        "../portfolio/img/Usecase diagram.png",
-        "../portfolio/img/실행 화면.png",
+        "img/ERD.png",
+        "img/Usecase diagram.png",
+        "img/실행 화면.png",
     ]
 
     // 이미지 확대 해주는 설정 포폴3
@@ -143,14 +143,14 @@ function pro(id, per){
     const $window1 = $(".window");
     const $window_content1 = $(".window-content");
     const img_list1 = [
-        "../portfolio/img/boold1.PNG",
-        "../portfolio/img/boold2.PNG",
-        "../portfolio/img/boold3.PNG",
-        "../portfolio/img/boold4.PNG",
-        "../portfolio/img/boold5.PNG",
-        "../portfolio/img/boold6.PNG",
-        "../portfolio/img/boold7.PNG",
-        "../portfolio/img/boold8.PNG"
+        "img/boold1.PNG",
+        "img/boold2.PNG",
+        "img/boold3.PNG",
+        "img/boold4.PNG",
+        "img/boold5.PNG",
+        "img/boold6.PNG",
+        "img/boold7.PNG",
+        "img/boold8.PNG"
     ]
 
     // 이미지 확대 해주는 설정 포폴4
@@ -158,13 +158,13 @@ function pro(id, per){
         const $window2 = $(".window");
         const $window_content2 = $(".window-content");
         const img_list2 = [
-            "../portfolio/img/jejuERD.PNG",
-            "../portfolio/img/jejuusecase.PNG",
-            "../portfolio/img/jejumain.PNG",
-            "../portfolio/img/jeju1.PNG",
-            "../portfolio/img/jeju2.PNG",
-            "../portfolio/img/jeju3.PNG",
-            "../portfolio/img/jeju4.PNG"
+            "img/jejuERD.PNG",
+            "img/jejuusecase.PNG",
+            "img/jejumain.PNG",
+            "img/jeju1.PNG",
+            "img/jeju2.PNG",
+            "img/jeju3.PNG",
+            "img/jeju4.PNG"
     ]
 
     //포폴2
